ac4df620070052c7ec3464baf95e4c0b531bbf25
Daniel Rocha <daniel1990rocha@gmail.com>
2021-08-12 18:10:42 -0300

    first commit

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,19 @@
+class Rack::Attack
+  Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+
+  safelist('allow-localhost') do |req|
+    '127.0.0.1' == req.ip || '::1' == req.ip
+  end
+
+  # Dando um limite de requisicoes
+  throttle('req/ip', limit: 5, period: 5) do |req|
+    req.ip
+  end
+
+  # Limitando quantidades de logins por um mesmo email
+  #   throttle('logins/email', limit: 5, period: 20.seconds) do |req|
+  #     if req.path == '/users/sign_in' && req.post?
+  #     req.params['email'].presence
+  #     end
+  #   end
+end
